155c5ab57512a0dd6106bdbab2d26f932ef5981b
Aterocana <dominicimaurizio@gmail.com>
2015-02-18 15:42:54 +0100

    init script fixed

--- init.sh
+++ init.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-FILES="*"
-dest="~"
-
-for f in $FILES
-do
-	if [ "$f" != "init.sh" ] && [ "$f" != "README.md" ] && [ "$f" != "conky-colors-configuration" ] ; then
-		move_files="$move_files $f"
-	fi
-done
-
-echo -e "Do you want to copy this files to $dest directory?\n $move_files  "
+echo "Copying configuration files"
+dest="~/"
+find . -type f -print0 | fgrep -zvf ignore.txt | grep -zv $0 | xargs -0 | sed -e "s/ /\n/g" | sort
+echo -e "Do you want to copy these files to the \"$dest\" directory?"
 read -p "[y/N] " -n 1 -r
 echo #new line
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	for f in $move_files
-	do			
-		cp -R $f $dest/ && echo "$f copied to $dest/$f."	
-	done
+	find . -type f -print0 | fgrep -zvf ignore.txt | grep -zv $0 | xargs -0 cp --parents -Rt $dest
 fi
