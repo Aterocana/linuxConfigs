4cf4319f355a6beed6d2fc24e56188cdf6691212
Aterocana <dominicimaurizio@gmail.com>
2015-09-24 14:29:35 +0200

    lemp

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -17,12 +17,10 @@ alias proc='ps -aux | grep'
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
 
 # Services
-alias lamp='sudo systemctl start httpd'
-alias lamp-stop='sudo systemctl stop httpd'
-alias lamp-restart='sudo systemctl restart httpd'
 alias db='sudo systemctl start mysqld'
 alias db-stop='sudo systemctl stop mysqld'
 alias db-restart='sudo systemctl restart mysqld'
 alias mongodb='sudo systemctl start mongodb'
 alias mongodb-stop='sudo systemctl stop mongodb'
 alias mongodb-restart='sudo systemctl restart mongodb'
+alias lemp='sudo systemctl start mysqld && sudo systemctl start php-fpm && sudo systemctl start nginx'
