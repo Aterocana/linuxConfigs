1d01ae99b12db4f1ab4c407d57787051da275a2e
Aterocana <dominicimaurizio@gmail.com>
2022-04-14 12:44:09 +0200

    git config preferences updated alias cat='bat'
    Signed-off-by: Aterocana <dominicimaurizio@gmail.com>

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -12,10 +12,16 @@ alias clean-cache='sudo pacman -Scc'
 alias unzip='extract'
 alias ll='ls -lh'
 alias lh='ll -ah'
-alias c='clear'
+alias cat='bat'
 alias proc='ps -aux | grep'
 alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
 alias set-python2="sudo unlink /usr/bin/python && sudo ln -s /usr/bin/python2 /usr/bin/python"
 alias set-python3="sudo unlink /usr/bin/python && sudo ln -s /usr/bin/python3 /usr/bin/python"
 alias vim='nvim'
 alias docker-rm-dangling='docker rmi -f $(docker images -f "dangling=true" -q)'
+
+# Services
+alias lamp='sudo systemctl start mariadb && sudo systemctl start httpd'
+alias lamp-stop='sudo systemctl stop mariadb && sudo systemctl stop httpd'
+#alias vpn='sudo openvpn --config /home/mauri/Documents/VPN/client.ovpn --auth-user-pass /home/mauri/Documents/VPN/credentials'
+alias vpn='sudo openfortivpn -c /home/mauri/Documents/VPN/forticlient < /home/mauri/Documents/VPN/pwd'
